22485acd960b00fd600ecc784cbd628fc0a5569b
manueldev <manuedit@gmail.com>
2014-11-15 17:56:25 -0300

    Codigo reescrito. Nuevo: compartir albumes, artistas y lista de seleccionados.

--- src/com/malejandrodev/addartisttitleforspotifyshare/MainActivity.java
+++ src/com/malejandrodev/addartisttitleforspotifyshare/MainActivity.java
@@ -1,88 +1,49 @@
 package com.malejandrodev.addartisttitleforspotifyshare;
 
-import org.apache.http.Header;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.loopj.android.http.*;
 
 public class MainActivity extends Activity {
-
-	String trackID, artista, titulo, link;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		trackID = getTrackIDFromIntent();
+		//Remove Notificacions
+		NotificationManager notifManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+		notifManager.cancelAll();
 		
-		if(trackID != null){
-			getSongInfo(trackID);
-		}else{
-			Toast.makeText(getApplicationContext(), getString(R.string.error_notrackid), Toast.LENGTH_SHORT).show();
-		}
+		String url = getLinkFromIntent();
+		if(url == null){
+			url = "nourl";
+		}		
+		new PublishItem(getApplicationContext()).execute(url);
 		this.finish();
 	}
 	
-	private void getSongInfo(String trackID) {
-		AsyncHttpClient client = new AsyncHttpClient();
-		client.get("https://api.spotify.com/v1/tracks/" + trackID, new JsonHttpResponseHandler(){
-
-			@Override
-			public void onSuccess(int statusCode, Header[] headers,
-					JSONObject response) {
-				try {
-					titulo = response.getString("name");
-					JSONArray artistasJSONArray = response.getJSONArray("artists");
-					JSONObject artistasJSONObject = artistasJSONArray.getJSONObject(0);
-					artista = artistasJSONObject.getString("name");
-					
-					if (BuildConfig.DEBUG) {
-						Log.i(getString(R.string.app_name), artista + " " + titulo);
-					}
-					enviarCancion("♫ " + titulo + " - " + artista + " " + link + " #NowPlaying");
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}	
-			}
-
-			@Override
-			public void onFailure(int statusCode, Header[] headers,
-					Throwable throwable, JSONObject errorResponse) {
-				Toast.makeText(getApplicationContext(), getString(R.string.error_nosonginfo), Toast.LENGTH_SHORT).show();
-			}
-		});
-	}
-
-	private String getTrackIDFromIntent() {
+	private String getLinkFromIntent() {
 		Intent receivedIntent = getIntent();
 		String receivedAction = receivedIntent.getAction();
+		
+
+		
+		
 		if(receivedAction.equals(Intent.ACTION_SEND)){
-			String receivedText = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
-			if (receivedText != null){
-				link  = receivedText;
-				String[] receivedText2 = receivedText.split("/");
-				return receivedText2[receivedText2.length-1];
-			}			
+			String rv = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
+			if (rv.startsWith("http://open.spotify.com/")) {
+				return rv;
+			} else {
+				return null;
+			}
+		}else if (BuildConfig.DEBUG){
+			return "http://open.spotify.com/user/0qgiFuYhYuwtFXEwYakddE/starred";
 		}
-		return null;
-	}
-	
-	public void enviarCancion(String txPublicar){
-		//txPublicar debe incluir "titulo – artista link #NowPlaying"
-		Intent sendIntent = new Intent();
-		sendIntent.setAction(Intent.ACTION_SEND);
-		sendIntent.putExtra(Intent.EXTRA_TEXT, txPublicar);
-		sendIntent.setType("text/plain");
-		startActivity(sendIntent);
-	}
+		else{
+			return null;
+		}	
+	}		
 }
---
+++ src/com/malejandrodev/addartisttitleforspotifyshare/PublishItem.java
@@ -0,0 +1,207 @@
+package com.malejandrodev.addartisttitleforspotifyshare;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+public class PublishItem extends AsyncTask<String, Void, Void>{
+	
+	/* 
+	 * Item Types
+	 * 
+	 * Track: ♫ Reina Japonesa - Fernando Milagros open.spotify.com/track/5OEqQkQ #NowPlaying
+	 * https://open.spotify.com/track/5OEqQkQKB37nPH30qyYHsA
+	 * /tracks/{id}
+	 *  
+	 * Artist: ♫ Tears For Fears spoti.fi/MVI4N1 #NowPlaying
+	 * https://open.spotify.com/artist/4bthk9UfsYUYdcFyqxmSUU?
+	 * /artists/{id} 
+	 * 
+	 * Album: ♫ The Seeds Of Love (Remastered with bonus tracks) – Tears For Fears spoti.fi/Q1ZxLc #NowPlaying
+	 * https://open.spotify.com/album/79XgUw5U86BeIiMnZ4Rrht
+	 * /albums/{id}
+	 * 
+	 * Starred list: ♫ Starred, Manuel Alejandro spoti.fi/1uQLjB2 #NowPlaying
+	 * http://open.spotify.com/user/1231232/starred
+	 * /users/{user_id} 
+	 * 
+	 * */
+
+	String link, title, artist, jsonr, item, mensaje = "♫ ";
+	String[] tipoSh = {
+            "tracks",
+            "artists",
+            "albums",
+            "starred"
+    	};
+	Integer mId = 0303456, tipoSelected;
+	Context context;
+	
+	public PublishItem(Context contextin){
+		context = contextin;
+	}
+	
+	@Override
+	protected Void doInBackground(String... arg0) {
+		link = arg0[0];
+		if(!isNetworkAvailable()){
+			showNotification(true, "Error", context.getString(R.string.error_noinet));
+		}else if(!parseLink() || link.equals("nourl")){
+			showNotification(false, "Error", context.getString(R.string.error_whenShFromWrongPlace));
+		}else if(!getitemInfo()){
+			String tipoShFromStrings[] = context.getResources().getStringArray(R.array.itemTypes);
+			showNotification(true, "Error", String.format(context.getString(R.string.error_noiteminfo), tipoShFromStrings[tipoSelected]));
+		}else if(!creatingMessage()){
+			showNotification(true, "Error", context.getString(R.string.error_parse));
+		}else{
+			enviarCancion(mensaje);
+		}
+		return null;
+	}
+	
+	private boolean creatingMessage() {
+		try {
+			JSONObject jsonParsed = new JSONObject(jsonr);
+			if (tipoSh[tipoSelected].equals("starred")){
+				String starredText = context.getResources().getStringArray(R.array.itemTypes)[3];
+				String owner = jsonParsed.getString("display_name");
+				mensaje += starredText.substring(0, 1).toUpperCase(Locale.getDefault()) + starredText.substring(1) + ", " + owner + " " + link + " #NowPlaying";
+			}else if(tipoSh[tipoSelected].equals("tracks") || tipoSh[tipoSelected].equals("albums")){
+				title = jsonParsed.getString("name");
+				JSONArray artistsJSONArray = jsonParsed.getJSONArray("artists");
+				for (int i = 0; i < artistsJSONArray.length(); i++){
+					if(i == 0){
+						artist = artistsJSONArray.getJSONObject(i).getString("name");
+					}else{
+						artist += ", " + artistsJSONArray.getJSONObject(i).getString("name");
+					}
+				}
+				mensaje += title + " - " + artist + " " + link + " #NowPlaying";
+			}else if(tipoSh[tipoSelected].equals("artists")){
+				artist = jsonParsed.getString("name");
+				mensaje += artist + " " + link + " #NowPlaying";
+			}
+			return true;
+		} catch (Exception e) {
+			Log.e(context.getString(R.string.app_name), e.getMessage());
+			return false;
+		}
+	}	
+
+	private boolean isNetworkAvailable() {
+	    ConnectivityManager connectivityManager 
+	          = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
+	
+	private void showNotification(boolean isError, String... info){
+		//info: 0 title, 1 description,
+		String tickerText;
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+		mBuilder
+        .setSmallIcon(R.drawable.ic_stat_notify_default)
+        .setContentTitle(info[0]);
+		tickerText = info[0];
+		if (info.length == 2){
+			mBuilder
+		        .setContentText(info[1])
+		        .setStyle(new NotificationCompat.BigTextStyle()
+		        .bigText(info[1]));
+			tickerText = info[1];
+		}
+		if (isError){
+			Intent intent = new Intent(context, MainActivity.class);
+			intent.putExtra(Intent.EXTRA_TEXT, link);
+			//ARRG
+			intent.putExtra("retryMode", true);
+			intent.setAction(Intent.ACTION_SEND);
+			intent.setType("text/plain");
+			PendingIntent resendIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+			mBuilder.addAction(R.drawable.ic_action_refresh, context.getString(R.string.retry), resendIntent);
+		}
+		
+		mBuilder.setTicker(tickerText);
+		NotificationManagerCompat mNotificationManager =
+				NotificationManagerCompat.from(context);
+		mNotificationManager.notify(null, mId, mBuilder.build());
+	}
+	
+	private boolean parseLink() {
+		try {
+			String[] linkDivido = link.split("/");
+			String tipoDeItem = linkDivido[linkDivido.length-2];
+			tipoDeItem += "s";		
+			tipoSelected = Arrays.asList(tipoSh).indexOf(tipoDeItem);
+			if(linkDivido[linkDivido.length-1].equals("starred")){
+				item = linkDivido[linkDivido.length-2];
+				tipoSelected = Arrays.asList(tipoSh).indexOf("starred");
+			}else{
+				item = linkDivido[linkDivido.length-1];
+			}
+			if (tipoSelected < 0){
+				return false;
+			}
+			return true;
+		} catch (Exception e) {
+			return false;
+		}		
+	}
+
+	private void enviarCancion(String txPublicar){
+		Intent sendIntent = new Intent();
+		sendIntent.setAction(Intent.ACTION_SEND);
+		sendIntent.putExtra(Intent.EXTRA_TEXT, txPublicar);
+		sendIntent.setType("text/plain");
+		sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(sendIntent);
+	}
+
+	private void loadData(String url) throws Exception {
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder()
+        .url(url)
+        .build();
+
+	    Response response = client.newCall(request).execute();
+	    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+	    jsonr = response.body().string();
+	}
+	
+	private boolean getitemInfo() {
+		
+		String apiUrl = "https://api.spotify.com/v1/";
+		if (tipoSh[tipoSelected].equals("starred")){
+			apiUrl += "users/" + item; 
+		}else{
+			apiUrl += tipoSh[tipoSelected] + "/" + item; 
+		}
+		
+		try {
+			loadData(apiUrl);
+			return true;
+		} catch (Exception e) {
+			Log.w(context.getString(R.string.app_name), e.getMessage());
+			return false;
+		}
+
+	}
+	
+}
